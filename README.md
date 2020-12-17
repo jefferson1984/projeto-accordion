d2255d9c2a8eb75df21093de8ca02758ee5194c0
Jefferson A. Silva <jeff2248@hotmail.com>
2020-12-17 18:30:26 -0300

    projeto finalizado

---
+++ README.md
@@ -0,0 +1,31 @@
+## Projeto Avaliações.
+
+Objetivo é utilização do react em uma simples aplicação e utilizar alguns conceitos para estudos.
+
+## Idea
+
+[uidesigndaily](https://uidesigndaily.com/posts/sketch-accordion-website-day-1175)
+
+![](./idea.png);
+
+## React Icons
+
+[react icons](https://react-icons.github.io/react-icons/)
+
+## Hooks
+
+useState useEffect e Condicional
+
+## Imagem do Projeto
+
+[projeto-avaliacoes.netlify.app](https://projeto-avaliacoes.netlify.app/)
+
+<img src="https://github.com/jefferson1984/projeto-avaliacoes/blob/main/avaliacoes2.png">
+
+## Reproduzir
+
+<ul>
+  <li>Para replicar o projeto em sua máquina local siga o seguintes passos:</li>
+  <li>Clone o repositório ou faça download do zip,  abra o terminar na pasta do projeto</li>
+  <li>Em seguida digite o seguintes comandos no terminal  <strong>npm install</strong> para instalar as dependências do projeto e logo depois  <strong>npm start</strong> para iniciar servidor local.</li>
+</ul>
---
+++ src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: "Devo permitir o uso de cookies?",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id felis quis ante sagittis pharetra. Fusce et tempor felis, eget sodales neque. Aliquam non iaculis sapien. Vestibulum vestibulum, nibh eget malesuada euismod, risus dolor fermentum sapien, ut pellentesque libero nisi sed dui. Vestibulum sit amet metus mi. ",
+  },
+  {
+    id: 2,
+    title: "Como altero a senha da minha página?",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id felis quis ante sagittis pharetra. Fusce et tempor felis, eget sodales neque. Aliquam non iaculis sapien. Vestibulum vestibulum, nibh eget malesuada euismod, risus dolor fermentum sapien, ut pellentesque libero nisi sed dui. Vestibulum sit amet metus mi. ",
+  },
+  {
+    id: 3,
+    title: "O que é BankID?",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id felis quis ante sagittis pharetra. Fusce et tempor felis, eget sodales neque. Aliquam non iaculis sapien. Vestibulum vestibulum, nibh eget malesuada euismod, risus dolor fermentum sapien, ut pellentesque libero nisi sed dui. Vestibulum sit amet metus mi. ",
+  },
+  {
+    id: 4,
+    title: "De quem é o número de nascimento que posso usar?",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id felis quis ante sagittis pharetra. Fusce et tempor felis, eget sodales neque. Aliquam non iaculis sapien. Vestibulum vestibulum, nibh eget malesuada euismod, risus dolor fermentum sapien, ut pellentesque libero nisi sed dui. Vestibulum sit amet metus mi. ",
+  },
+  {
+    id: 5,
+    title: "Quando recebo uma senha solicitada por carta?",
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id felis quis ante sagittis pharetra. Fusce et tempor felis, eget sodales neque. Aliquam non iaculis sapien. Vestibulum vestibulum, nibh eget malesuada euismod, risus dolor fermentum sapien, ut pellentesque libero nisi sed dui. Vestibulum sit amet metus mi. ",
+  },
+];
+export default questions;
